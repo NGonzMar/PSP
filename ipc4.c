debeb3f49af23ff767c305dd9b41794b7d3a879b
Nuria González Margallo <nuriagonzalezmargallo@gmail.com>
2022-10-25 10:31:04 +0200

    Update ipc4.c

--- ipc4.c
+++ ipc4.c
@@ -5,6 +5,12 @@
 #include <time.h> 
 #include <time.h>
 
+// Funcion que devuelve el factorial de un numero | Generada fuera de main para completo acceso 
+int factorial(int n){   
+    if(n == 0) return 1;
+    else return n * factorial(n-1);
+} 
+
 void main(){
 				
      int fd[2];	
@@ -12,12 +18,6 @@ void main(){
      int buffer[30];
 
      pid_t pid; // Proceso padre
-
-     // Generacion de numero aleatorio
-     time_t t;
-     int numero1;
-     srand((unsigned) time(&t));
-     numero1=rand() % 10; 
 
      // Creamos el PIPE1
      pipe(fd); 
@@ -28,26 +28,18 @@ void main(){
      // Se crea un proceso hijo
      pid = fork();
 
-
-    // Funcion que devuelve el factorial de un numero
-    int factorial(int n)
-    {   
-        if(n == 0) return 1;
-        else return n * factorial(n-1);
-    } 
-
-
     if (pid==0)
     {
         // El hijo lee el PIPE1
         close(fd[1]); // Cierra el descriptor de escritura
+        close(fd2[0]); // Cierra el descriptor de lectura dos
+
         read(fd[0], buffer, sizeof(int));
         write(fd[0], buffer, sizeof(int));
         printf("El proceso padre genera el numero %d en el pipe1\n", buffer[0]);
                 
         // El hijo calcula el factorial y lo escribe en el PIPE2        
-        int resultadoFactorial = factorial(buffer[0]);
-        close(fd2[0]); // Cierra el descriptor de lectura dos
+        int resultadoFactorial = factorial(buffer[0]);   
         write(fd2[1], &resultadoFactorial, sizeof(int));
      
     }
@@ -55,13 +47,26 @@ void main(){
      else
      
      {
+        // Generacion de numero aleatorio
+        int numAleatorio()
+        {
+            time_t t;
+            int numero1;
+            srand((unsigned) time(&t));
+            numero1=rand() % 10; 
+
+            return numero1;
+        }
+
      	// El numero se escribe en el PIPE1
         close(fd[0]); // Cierra el descriptor de lectura
+        close(fd2[1]); // Cierra el descriptor de escritura dos  
+
+        int numero1 = numAleatorio();
         write(fd[1], &numero1, sizeof(int)); 
         wait(NULL); 
                 
         // El padre lee el PIPE2 y muestra el factorial del numero
-        close(fd2[1]); // Cierra el descriptor de escritura dos  
         read(fd2[0], buffer, sizeof(int));	
         printf("El factorial calculado por el proceso hijo: %d\n", buffer[0]);
      }
