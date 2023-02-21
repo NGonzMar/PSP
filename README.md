ce8c9a885618672973b386eccd731887758d8eb0
NGonzMar <nuriagonzalezmargallo@gmail.com>
2023-02-21 10:05:10 +0100

    Subida de archivos finales

---
+++ README.md
@@ -0,0 +1,1 @@
+EJERCICIOS DE PROGRAMACION MULTIPROCESO (Programación de Servicios y Procesos) | 2º DAM
---
+++ ej11.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h> //libreria para system()
+#include <unistd.h> //libreria para sleep()
+
+int main()
+{
+  printf("\nNombre del alumno: Nuria González Margallo\n");
+  
+  printf("\nNombre del usuario: \n");
+  system("whoami");
+  
+  printf("\nContenido del directorio '/home/alumno/Descargas': \n");
+  system("ls -S /home/alumno/Descargas");
+  
+  printf("\nEl proceso se esta ejecutando en la terminal: \n");
+  system("tty");
+   
+  printf("\nMe voy a dormir 1 minuto... \n");
+  sleep(60);
+  printf("\n¡He despertado!\n");
+  
+  printf("\nInformacion del proceso:\n");
+  system("ps -ef | grep \"ej11\"");
+  
+  printf("\nArbol de procesos:\n");
+  system("pstree -p");
+  
+  printf("\nFin del programa.\n");
+}	
